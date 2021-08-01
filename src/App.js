4fcf98823a97e743c7e97ff9fee66f7240c2899b
nadiandnd <ndtaeminnie@gmail.com>
2021-08-01 20:58:51 +0700

    learn fragment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import NYCate from "./NYCate.webp";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+      <img 
+        src={NYCate}
+        height={200} 
+        alt="I love New York Pizza."
+      />
+      <ul style={{ textAlign: "left" }}>
+          {props.dishes.map((dish) => (
+            <li key={dish.id}>{dish.title}</li>
+          ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables",
+  "Pizza"
+]
+
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
+console.log(dishObjects);
 
 function App() {
   return (
+    // this can return only one fragment
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Nadia" />
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+
+function AppTwo() {
+  return <h1>This is the Second App</h1>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+    <AppTwo />
+  </>,
+   
+  // <></> is the short hand fragment of <React.Fragment></React.Fragment>
+
+  // <ul>
+  //   <li>Monday</li>
+  //   <li>Tuesday</li>
+  //   <li>Wednesday</li>
+  // </ul>,
+  // React.createElement(
+  //   "ul", 
+  //   { style: { color: "blue" } },
+  //   React.createElement("li", null, "Monday"),
+  //   React.createElement("li", null, "Tuesday"),
+  //   React.createElement("li", null, "Wednesday")
+  //   ),
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
